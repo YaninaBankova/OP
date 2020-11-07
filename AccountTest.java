5bb4af27b24c08188d9fd4762eae586d217143de
YaninaBankova <43926387+YaninaBankova@users.noreply.github.com>
2020-11-07 18:13:07 +0200

    Add files via upload

---
+++ AccountTest.java
@@ -0,0 +1,15 @@
+package edu.smg;
+
+public class AccountTest {
+	public static void main(String[] args) {
+		Account acc = new Account();
+		acc.setId(1122);
+		acc.setBalance(20_000);
+		acc.setYearInterestRate(4.5);
+		acc.withdraw(2500);
+		acc.deposit(3000);
+		
+		System.out.println("Balance: " + acc.getBalance() + "\nMonthly inerest: " + acc.getMonthlyInterest() + 
+				"\nDate: " + acc.getDateCreated());
+	}
+}
